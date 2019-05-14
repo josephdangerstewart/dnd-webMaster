ebb63adb100be8f676de43e0f89bf6bbafb39014
Joseph Stewart <joseph.s@nationalschoolproject.com>
2019-05-13 21:57:57 -0700

    Add global search API route

--- back-end/search/SearchController.js
+++ back-end/search/SearchController.js
@@ -1,3 +1,4 @@
+import Fuse from 'fuse.js';
 import {
 	promiseQuery,
 } from '../utility';
@@ -57,7 +58,7 @@ export const search = ({
 	let fieldsArray = [];
 	const customFields = [];
 	if (fields) {
-		fieldsArray = 
+		fieldsArray =
 			fields
 				.split(',')
 				.map(field => {
@@ -78,7 +79,7 @@ export const search = ({
 				SELECT
 					${idColumn},
 					${nameColumn}
-					${fieldsArray.length > 0 ? ', :(fieldsArray)' : '' }
+					${fieldsArray.length > 0 ? ', :(fieldsArray)' : ''}
 					${customFields.length > 0 ? `, ${customFields.join(',')}` : ''}
 				FROM
 					${tableName}
@@ -98,4 +99,156 @@ export const search = ({
 	);
 
 	return results;
+};
+
+export const globalSearch = async (path, queryString, user, connection) => {
+	const {
+		query,
+	} = queryString;
+	const { campaignID } = path;
+
+	// Define a default limit of 10 if one is not provided
+	let { count } = queryString;
+	if (!count) {
+		count = 10;
+	}
+
+	// Define the segment of the query that limits the results
+	let countSegment = '';
+	if (count && !isNaN(count)) {
+		countSegment = 'LIMIT :count';
+	}
+
+	// Define one object for SQL variables for convenience
+	const variables = {
+		query: `%${query}%`,
+		count,
+		campaignID,
+	};
+
+	const promises = [];
+
+	// Search spells
+	promises[0] = promiseQuery(
+		connection,
+		`
+			SELECT 
+				spellName AS name,
+				spellID AS id,
+				'spell' AS type
+			FROM spell
+			WHERE spellName LIKE :query
+			${countSegment}
+		`,
+		variables
+	);
+
+	// Search equipment
+	promises[1] = promiseQuery(
+		connection,
+		`
+			SELECT
+				equipmentName AS name,
+				equipmentID AS id,
+				'equipment' AS type
+			FROM equipment
+			WHERE equipmentName LIKE :query
+			${countSegment}
+		`,
+		variables
+	);
+
+	// Search class features
+	promises[2] = promiseQuery(
+		connection,
+		`
+			SELECT
+				klassName AS name,
+				klassID AS id,
+				'klass' AS type
+			FROM klass
+			WHERE klassName LIKE :query
+			${countSegment}
+		`,
+		variables
+	);
+
+	// Search characters
+	promises[3] = promiseQuery(
+		connection,
+		`
+			SELECT
+				characterName AS name,
+				\`character\`.characterID AS id,
+				'character' AS type
+			FROM
+				\`character\`
+				JOIN characterlist
+					ON \`character\`.characterID = characterlist.characterID
+			WHERE
+				characterName LIKE :query
+				AND
+				characterlist.campaignID = :campaignID
+			${countSegment}
+		`,
+		variables
+	);
+
+	// Search notes
+	promises[4] = promiseQuery(
+		connection,
+		`
+			SELECT
+				noteTitle AS name,
+				noteID AS id,
+				'note' AS type,
+				noteContent AS other
+			FROM note
+			WHERE
+				(noteTitle LIKE :query OR noteContent LIKE :query)
+				AND
+				campaignID = :campaignID
+			${countSegment}
+		`,
+		variables
+	);
+
+	// Search note folders
+	promises[5] = promiseQuery(
+		connection,
+		`
+			SELECT
+				folderName AS name,
+				noteFolderID AS id,
+				'folder' AS type
+			FROM notefolder
+			WHERE
+				folderName LIKE :query
+				AND
+				campaignID = :campaignID
+			${countSegment}
+		`,
+		variables
+	);
+
+	// Resolve all of the promises and flatten the array
+	const promiseResolution = (await Promise.all(promises)).flat();
+
+	// Do fuzy search
+	const options = {
+		shouldSort: true,
+		threshold: 0.6,
+		location: 0,
+		distance: 100,
+		maxPatternLength: 32,
+		minMatchCharLength: 1,
+		keys: [
+			'name',
+			'other',
+		],
+	};
+
+	const fuse = new Fuse(promiseResolution, options);
+
+	return fuse.search(query).filter((value, index) => index < count);
 };
