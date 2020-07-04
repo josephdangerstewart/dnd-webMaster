e2fa8d51189d2b966b06211aee0f9ff5fdc47067
Joseph Stewart <joseph.s@nationalschoolproject.com>
2020-07-04 15:11:44 -0700

    Put color picker in modal

--- front-end/src/components/tools/map-maker/custom-toolbar/menu-items/ColorPickerMenuItem.jsx
+++ front-end/src/components/tools/map-maker/custom-toolbar/menu-items/ColorPickerMenuItem.jsx
@@ -1,35 +1,56 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {
 	MenuItem,
 } from '@blueprintjs/core';
 import styles from './styles.less';
 import { ColorPicker } from '../../../../color-picker';
+import ActionModal from '../../../../modal/ActionModal';
 
 export function ColorPickerMenuItem({ title, value, setValue }) {
+	const [ isModalOpen, setIsModalOpen ] = useState(false);
+	const [ tempColor, setTempColor ] = useState(value);
+
+	const openModal = useCallback(() => setIsModalOpen(true), []);
+	const closeModal = useCallback(() => setIsModalOpen(false), []);
+
+	const onSubmit = useCallback(() => {
+		setValue(tempColor);
+		setIsModalOpen(false);
+	}, [ setValue, tempColor ]);
+
 	const color = value || 'rgba(0, 0, 0, 0)';
 	return (
-		<MenuItem
-			text={(
-				<div className={styles.menuItemTextContainer}>
-					<ColorPreview
-						color={color}
-					/>
-					<span>
-						{title}
-					</span>
-				</div>
-			)}
-			shouldDismissPopover={false}
-			popoverProps={{
-				captureDismiss: true,
-			}}
-		>
-			<ColorPicker
-				color={color}
-				setColor={setValue}
+		<>
+			<MenuItem
+				text={(
+					<div className={styles.menuItemTextContainer}>
+						<ColorPreview
+							color={color}
+						/>
+						<span>
+							{title}
+						</span>
+					</div>
+				)}
+				shouldDismissPopover={false}
+				popoverProps={{
+					captureDismiss: true,
+				}}
+				onClick={openModal}
 			/>
-		</MenuItem>
+			<ActionModal
+				open={isModalOpen}
+				onClose={closeModal}
+				onSubmit={onSubmit}
+				title={title}
+			>
+				<ColorPicker
+					color={tempColor}
+					setColor={setTempColor}
+				/>
+			</ActionModal>
+		</>
 	);
 }
 
